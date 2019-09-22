2508ef33e6e09afbd82d0ff0ee934d69d1a2c42e
oodennis20 <oodennis20@gmail.com>
2019-09-22 14:13:54 +0300

    added an input type for;reset

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,52 @@
-var day =["sunday","tuesday","wednesday","thursday","friday","saturday","monday"]
-var d = new Date(dateString);
-var dayName = days[d.getDay()];
-function calculator(){
-    var name=""
-    var gender=document.forms["calc"]["gender"].value;
-    var date=document.forms["calc"]["date"].value;
-    var day=dateobject.getDay();
-    var dateObject=new Date(date);
-if (gender==="male"){
-    name=male[day];
+var dayName = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"];
+var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var CC, YY, MM, d, name;
+  var year;
+  var month;
+  var day;
+  var gender;
+  function compile(){
+      validate();
+     alert ("Your Akan name is " +calculateDay());
+      findGender();
+  }
+function validate(){
+  var day= document.forms["calc"]["date"].value;
+  var month= document.forms["calc"]["month"].value;
+  var year= document.forms["calc"]["year"].value;
+  var gender= document.querySelector('input[name="gender"]:checked').value;
+  if(year==""){
+    document.getElementById("month").innerHTML ="Enter valid year"
+      return false;
+  }
+  if(month=="" || month<0 || month>12){
+    document.getElementById("month").innerHTML ="Enter valid month"
+      return false;
+  }
+  if(date=="" || date<0 || date>31){
+    document.getElementById("date").innerHTML ="Provide a valid date"
+      return false;
+  }
+  if(gn[0].checked== false && gn[1].checked==false){
+    document.getElementById("gender").innerHTML ="Please choose gender"
+      return false;
+  }
+  else{
+      return true;
+  }
 }
-else if (gender==="female"){
-    name=female[day]
-}
-document.getElementById("prompt").innerHTML=name;
-     return name;
-}
-//function validation(){
-    //var date = document.getElementById("date").value;
-   // var month = document.getElementById("month").value;
-   // var year = document.getElementById("year").valuecx;
-    if(date=="" || month=="" || year=="") {
-
-    document.getElementById("eresult").innerHTML = "All field required";
-    return;
-    } 
-    else if(date<0 && date>31){
-    return;
-    }
-    else if(month<0 && month>12){
-        document.getElementById("month").innerHTML = "Month Must Be  ";
-    return;
-    }
-    else if(year.length<4){
-        document.getElementById("year").innerHTML = "Year Must Be 4 characters";
-    return;
-    }
-        document.getElementById("submit").addEventListener = "Year Must Be 4 characters";
+function calculateDay(gender){
+  var name="";
+  year=document.forms["calc"]["year"].value;
+  MM=document.forms["calc"]["month"].value;
+  DD=document.forms["calc"]["date"].value;
+  var date= new Date(year+ "/" +MM +"/" +DD);
+  var day = date.getDay();
+  if (gender==="male"){
+      name=maleName[day]
+  }else{
+      name=femaleName[day]
+  }
+ return name;
 }
