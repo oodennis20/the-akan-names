55afc9525449427890a763dd1e0ec862d58c221b
oodennis20 <oodennis20@gmail.com>
2019-09-22 23:33:50 +0300

    added a README file

---
+++ README.md
@@ -0,0 +1,13 @@
+# The Ghanian Akan Names Application
+#### The Ghanian Culture(Akan Names), 21st september 2019
+#### By **Dennis Otieno**
+## Description
+This application shows one which akan name is assigned to them depending on the date of birth and gender given
+## Technologies Used
+the application uses html and java script 
+## BDD
+This 'Akan Names Engine' assigns Ghanian cultural names to the user either male or female. The user is assigned a particular akan name depending on the date of birth given and gender too. This engine then takes the information given then issues out the akan name to the user. When the user fills the fields given correctly then he/she will automatically be assigned an akan name. If the fields are not filled correctly by the user then the user will not be assigned any akan name.
+## Support and contact details
+Incase of any difficulties;oodennis@gmail.com
+### License
+MIT License Copyright (c) 2019 **Dennis Otieno**
--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,21 @@
 var dayName = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"];
 var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var CC, YY, MM, d, name;
-  var year;
-  var month;
-  var day;
-  var gender;
+var CC;
+  var YY;
+  var MM;
+  var date;
+  var name;
   function validate(){
      alert ("Your Akan name is " +calculateDay());
-      findGender(); 
-  }
-function validate(){
-  var day= document.getElementById("date").value;
-  var month= document.getElementById("month").value;
-  var year= document.getElementById("year").value;
-  var gender= document.querySelector('input[name="gender"]:checked').value;
-  if(year==""){
-    document.getElementById("month").innerHTML ="Enter valid year"
-      return false;
-  }
-  if(month=="" || month<0 || month>12){
-    document.getElementById("month").innerHTML ="Enter valid month"
-      return false;
-  }
-  if(date=="" || date<0 || date>31){
-    document.getElementById("date").innerHTML ="Provide a valid date"
-      return false;
-  }
-  if(gn[0].checked== false && gn[1].checked==false){
-    document.getElementById("gender").innerHTML ="Please choose gender"
-      return false;
-  }
-  else{
-      return true;
-  }
-}
-function calculateDay(gender){
+      findGender();
+function calculateDay(gender,month,year,date){
   var name="";
-  year=document.forms["calc"]["year"].value;
-  MM=document.forms["calc"]["month"].value;
-  DD=document.forms["calc"]["date"].value;
+  year=document.getElementById("year").value;
+  MM=document.getElementById("month").value;
+  DD=document.getElementById("date").value;
   var date= new Date(year+ "/" +MM +"/" +DD);
-  var day = date.getDay();
+  var day = date.calculateDay();
   if (gender==="male"){
       name=maleName[day]
   }else{
